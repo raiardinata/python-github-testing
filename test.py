1a6d8f336e9e1d27cfaa777b5d7a31a1b4398319
raiardinata <raiardinata@gmail.com>
2024-03-26 10:20:23 +0700

    initiate repo

---
+++ test.py
@@ -0,0 +1,14 @@
+def primes(n: int):
+    sieve = [True] * n
+    res = []
+    for i in range(2, n):
+        if sieve[i]:
+            res.append(i)
+            for j in range(i * i, n, i):
+                sieve[j] = False
+    return res
+
+
+x5 = primes(180)
+
+print(x5)
